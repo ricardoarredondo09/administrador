9d084a062492c3df034950179a1b25a3f34d7c91
Ricardo <ricard.arredondo@gmail.com>
2021-04-18 00:35:46 -0400

    Se modifica Nombre del proyecto, solo usuario registrador puede agregar mas usuarios al sitema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,11 +43,12 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
+                                @auth
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                                @endauth
+
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
